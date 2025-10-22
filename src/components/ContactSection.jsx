3e145d74499713398d8c22e5c36fa44648d9d5aa
Mayank1094 <mayankkamble1094@gmail.com>
2025-10-22 16:42:34 +0530

    ContactSection.jsx

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -94,7 +94,7 @@ export const ContactSection = () => {
               <h4 className="font-medium mb-4"> Connect With Me</h4>
               <div className="flex space-x-4 justify-center">
                 <a href="#" target="_blank">
-                  <Whatsapp />
+                  <Linkedin />
                 </a>
                 <a href="#" target="_blank">
                   <Twitter />
